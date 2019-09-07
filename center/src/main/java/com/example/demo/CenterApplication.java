b0afc9d2c1aafb9a5c8c4f0e51c00b4454c47515
kylesun <457799096@qq.com>
2019-09-07 20:57:31 +0800

    new commit

--- center/src/main/java/com/example/demo/CenterApplication.java
+++ center/src/main/java/com/example/demo/CenterApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class DemoApplication {
+public class CenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(CenterApplication.class, args);
     }
 
 }
